c5dc43a8a07e32dc7d2a87d11815a5eb53ec4067
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-08-25 11:38:31 +0300

    Add logical device and queues

--- src/main.rs
+++ src/main.rs
@@ -30,14 +30,16 @@ const VALIDATION_LAYER: vk::ExtensionName =
 
 #[derive(Default)]
 struct App {
-    window: Option<Window>,
+    data: Option<AppData>,
+    device: Option<Device>,
     entry: Option<Entry>,
     instance: Option<Instance>,
-    data: Option<AppData>,
+    window: Option<Window>,
 }
 
 #[derive(Default)]
 struct AppData {
+    graphics_queue: vk::Queue,
     messenger: vk::DebugUtilsMessengerEXT,
     physical_device: vk::PhysicalDevice,
 }
@@ -91,6 +93,7 @@ impl App {
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
         let mut data = AppData::default();
         let instance = create_instance(&window, &entry, &mut data)?;
+        let device = create_logical_device(&entry, &instance, &mut data)?;
 
         pick_physical_device(&instance, &mut data)?;
 
@@ -98,17 +101,20 @@ impl App {
         self.entry = Some(entry);
         self.instance = Some(instance);
         self.data = Some(data);
+        self.device = Some(device);
 
         Ok(())
     }
 
     unsafe fn destroy(&mut self) {
-        let instance = self.instance.as_mut().unwrap();
+        let instance = self.instance.as_ref().unwrap();
+        let device = self.device.as_ref().unwrap();
 
         if VALIDATION_ENABLED {
             instance.destroy_debug_utils_messenger_ext(self.data.as_ref().unwrap().messenger, None);
         }
 
+        device.destroy_device(None);
         instance.destroy_instance(None);
     }
 }
@@ -251,6 +257,47 @@ unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Resul
     }
 
     Err(anyhow!("Failed to find suitable physical device."))
+}
+
+unsafe fn create_logical_device(
+    entry: &Entry,
+    instance: &Instance,
+    data: &mut AppData,
+) -> Result<Device> {
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+    let queue_priorities = &[1.0];
+    let queue_info = vk::DeviceQueueCreateInfo::builder()
+        .queue_family_index(indices.graphics)
+        .queue_priorities(queue_priorities);
+
+    let layers = if VALIDATION_ENABLED {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        vec![]
+    };
+
+    let mut extensions = vec![];
+
+    // Required by Vulkan SDK on macOS since 1.3.216.
+    if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+        extensions.push(vk::KHR_PORTABILITY_SUBSET_EXTENSION.name.as_ptr());
+    }
+
+    let features = vk::PhysicalDeviceFeatures::builder();
+
+    let queue_infos = &[queue_info];
+    let info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(queue_infos)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions)
+        .enabled_features(&features);
+
+    let device = instance.create_device(data.physical_device, &info, None)?;
+
+    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+    Ok(device)
 }
 
 extern "system" fn debug_callback(
