06a12b5af1e2bda5b82128ec7f6cdaff2a4d89b9
Stanislav Shandyga <stanislavshandyga@gmail.com>
2024-08-26 11:48:00 +0300

    Add command buffers

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,8 @@ struct App {
 
 #[derive(Default)]
 struct AppData {
+    command_buffers: Vec<vk::CommandBuffer>,
+    command_pool: vk::CommandPool,
     framebuffers: Vec<vk::Framebuffer>,
     graphics_queue: vk::Queue,
     messenger: vk::DebugUtilsMessengerEXT,
@@ -123,6 +125,8 @@ impl App {
         create_render_pass(&instance, &device, &mut data)?;
         create_pipeline(&device, &mut data)?;
         create_framebuffers(&device, &mut data)?;
+        create_command_pool(&instance, &device, &mut data)?;
+        create_command_buffers(&device, &mut data)?;
 
         self.window = Some(window);
         self.entry = Some(entry);
@@ -147,6 +151,7 @@ impl App {
             .iter()
             .for_each(|f| device.destroy_framebuffer(*f, None));
 
+        device.destroy_command_pool(data.command_pool, None);
         device.destroy_pipeline(data.pipeline, None);
         device.destroy_render_pass(data.render_pass, None);
         device.destroy_pipeline_layout(data.pipeline_layout, None);
@@ -656,6 +661,67 @@ unsafe fn create_framebuffers(device: &Device, data: &mut AppData) -> Result<()>
             device.create_framebuffer(&create_info, None)
         })
         .collect::<Result<Vec<_>, _>>()?;
+
+    Ok(())
+}
+
+unsafe fn create_command_pool(
+    instance: &Instance,
+    device: &Device,
+    data: &mut AppData,
+) -> Result<()> {
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+    let info = vk::CommandPoolCreateInfo::builder()
+        .flags(vk::CommandPoolCreateFlags::empty()) // Optional.
+        .queue_family_index(indices.graphics);
+
+    data.command_pool = device.create_command_pool(&info, None)?;
+
+    Ok(())
+}
+
+unsafe fn create_command_buffers(device: &Device, data: &mut AppData) -> Result<()> {
+    let allocate_info = vk::CommandBufferAllocateInfo::builder()
+        .command_pool(data.command_pool)
+        .level(vk::CommandBufferLevel::PRIMARY)
+        .command_buffer_count(data.framebuffers.len() as u32);
+
+    data.command_buffers = device.allocate_command_buffers(&allocate_info)?;
+
+    for (i, command_buffer) in data.command_buffers.iter().enumerate() {
+        let info = vk::CommandBufferBeginInfo::builder();
+
+        device.begin_command_buffer(*command_buffer, &info)?;
+
+        let render_area = vk::Rect2D::builder()
+            .offset(vk::Offset2D::default())
+            .extent(data.swapchain_extent);
+
+        let color_clear_value = vk::ClearValue {
+            color: vk::ClearColorValue {
+                float32: [0.0, 0.0, 0.0, 1.0],
+            },
+        };
+
+        let clear_values = &[color_clear_value];
+        let info = vk::RenderPassBeginInfo::builder()
+            .render_pass(data.render_pass)
+            .framebuffer(data.framebuffers[i])
+            .render_area(render_area)
+            .clear_values(clear_values);
+
+        device.cmd_begin_render_pass(*command_buffer, &info, vk::SubpassContents::INLINE);
+        device.cmd_bind_pipeline(
+            *command_buffer,
+            vk::PipelineBindPoint::GRAPHICS,
+            data.pipeline,
+        );
+        device.cmd_draw(*command_buffer, 3, 1, 0, 0);
+        device.cmd_end_render_pass(*command_buffer);
+
+        device.end_command_buffer(*command_buffer)?;
+    }
 
     Ok(())
 }
